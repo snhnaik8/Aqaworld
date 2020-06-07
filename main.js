1587feb98c33cbbde97e7db1bb54b4eb1f4e5a12
snhnaik8 <snhnaik8@gmail.com>
2020-06-07 14:30:19 +0530

    commit 3

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 window.onscroll = function () { scrollFunction() };
-
+//Function to make nav white and reduce logosize on scroll 
 function scrollFunction() {
 
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
@@ -24,8 +24,7 @@ function scrollFunction() {
 
     }
 }
-
-
+//on click of navabar take to the particular section
 $(".navbar a").click(function () {
     $("body,html").animate({
         scrollTop: $("#" + $(this).data('value')).offset().top - 75
@@ -33,7 +32,7 @@ $(".navbar a").click(function () {
     //added offset -75 to get to the correct id
 })
 
-
+//responsive site to make logo small 
 $(window).resize(function () {
     if (document.body.clientWidth > 576) {
         $(".main-logo").css("width", "90px");
@@ -43,19 +42,19 @@ $(window).resize(function () {
     }
 });
 
+//to hide navabar once click once collapse
 $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
-$("#img01").on("click", function() {
-    $(this).modal();
- });
+// $("#img01").on("click", function () {
+//     $(this).modal();
+// });
 
- function myFunction(src,text)
- {
-console.log(src);
-$("#imgModal").attr("src","img/gallery"+ src +".jpg");
-$('#exampleModalLabel').html(text);
- }
+//modal
+function myFunction(src, text) {
+    console.log(src);
+    $("#imgModal").attr("src", "img/gallery" + src + ".jpg");
+    $('#exampleModalLabel').html(text);
+}
 
- 
